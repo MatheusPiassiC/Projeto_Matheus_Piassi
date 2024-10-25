0ab23f4842b5ff1ce6c73fad17730aa2a5bcf8d4
Matheus Piassi <matheuspiassi4@gmail.com>
2024-10-25 18:09:22 -0300

    questao 7 2.0

---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+
+    public void setB2(float b2) {
+        B2 = b2;
+    }
+
+    public String MB1(){
+        return "MB1";
+    }
+
+    public String MB2(){
+        return "MB2";
+    }
+}
