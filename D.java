2cc4c2bcccf13d79e1c67f323c7bce32d6ac7b5c
Matheus Piassi <matheuspiassi4@gmail.com>
2024-10-26 15:55:02 -0300

    ciração da classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public float getD2() {
+        return D2;
+    }
+
+    public void setD1(int d1) {
+        D1 = d1;
+    }
+
+    public void setD2(float d2) {
+        D2 = d2;
+    }
+
+    public int MD1(){
+        return D1*2;
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
